fe7d46d81cbda217a2aadeaef258b2892ef904a6
dakkang <97874131+dakkang03@users.noreply.github.com>
2024-11-27 21:18:38 -0700

    Update LabP13_Exercise4_3.m

--- LabP13_Exercise4_3.m
+++ LabP13_Exercise4_3.m
@@ -1,21 +1,24 @@
-% Test script for dtmfrun
+%% Test Script for dtmfrun.m
 
 % Sampling frequency
 fs = 8000; % Use this sampling rate in all functions
 
 % Define the sequence of DTMF keys
-tk = ['A', 'B', 'C', 'D', '*', '#', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+tk = ['4', '0', '7', '*', '8', '9', '1', '3', '2', '#', 'B', 'A', 'D', 'C'];
 
 % Generate a DTMF signal for the test sequence using dtmfdial
 % Assuming dtmfdial is available or you have the signal pre-generated
-xx = dtmfdial(tk, fs); % Replace this with your signal if pre-generated
-
+xx = dtmfdial(tk, fs); % Replace this with your signal if pre-generate
 % Define the filter length
 L = 80; % Adjust this value as required by your implementation
 
 % Test the dtmfrun function
 disp('Testing dtmfrun...');
 decoded_keys = dtmfrun(xx, L, fs);
+
+% Display the original keys
+disp('Original keys:');
+disp(tk);
 
 % Display the decoded keys
 disp('Decoded keys:');
